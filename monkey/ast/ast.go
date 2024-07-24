b3c7ab9fb0d9d646c8e49432d76e9c4c76b30de7
Julian Orlowski <julian.sz.orlowski@gmail.com>
2024-07-24 16:07:37 +0200

    Parsing infix expressions

--- monkey/ast/ast.go
+++ monkey/ast/ast.go
@@ -75,6 +75,26 @@ type ExpressionStatement struct {
 func (es *ExpressionStatement) statementNode() {}
 func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
 
+type PrefixExpression struct {
+	Token	token.Token
+	Operator	string
+	Right	Expression
+}
+
+func (pe *PrefixExpression) expressionNode() {}
+func (pe *PrefixExpression)  TokenLiteral() string {return pe.Token.Literal }
+func (pe *PrefixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(pe.Operator)
+	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+
+}
+
 type IntegerLiteral struct {
 	Token token.Token
 	Value int64

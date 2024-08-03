c9f6b43639a0e1b39967cff4c6fbd167abafff7e
Julian Orlowski <julian.sz.orlowski@gmail.com>
2024-08-03 18:37:07 +0200

    BASIC OBJECT SYSTEM THIS IS SO FUN I LOVE IT! Now repl actually evaluates!

---
+++ monkey/evaluator/evaluator.go
@@ -0,0 +1,84 @@
+package evaluator
+
+import (
+	"BadlyWrittenStylesheet/Monkey/monkey/ast"
+	"BadlyWrittenStylesheet/Monkey/monkey/object"
+)
+
+var (
+	TRUE	= &object.Boolean{Value: true}
+	FALSE	= &object.Boolean{Value: false}
+	NULL	= &object.Null{}
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+
+	// Stmts
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
+
+	// Exprss
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
+	}
+
+	return nil
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+
+	return result
+}
---
+++ monkey/evaluator/evaluator_test.go
@@ -0,0 +1,97 @@
+package evaluator
+
+import (
+	"BadlyWrittenStylesheet/Monkey/monkey/lexer"
+	"BadlyWrittenStylesheet/Monkey/monkey/object"
+	"BadlyWrittenStylesheet/Monkey/monkey/parser"
+	"testing"
+)
+
+func TestBangOperator(t *testing.T) {
+	tests := []struct {
+		input string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!5", false},
+		{"!!true", true},
+		{"!!false", false},
+		{"!!5", true},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+func TestBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Errorf("objewct is not Boolean. got=%T %+v", obj, obj)
+		return false
+	}
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%t, want=%t", obj, obj)
+		return false
+	}
+
+	return true
+}
+
+func TestEvalIntegerExpression(t *testing.T) {
+	tests := []struct {
+		input string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+		{"-5", -5},
+		{"-10", -10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	return Eval(program)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		t.Errorf("object is not Integer. got=%T (%+v)", obj, obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
+		return false
+	}
+
+	return true
+}
+

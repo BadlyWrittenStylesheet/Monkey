d157a38689b4b16439a712301b0715c615d64dc5
Julian Orlowski <julian.sz.orlowski@gmail.com>
2024-08-17 11:34:41 +0200

    Builtin functions for arrays. Added hashes ( pre eval )

--- monkey/evaluator/evaluator.go
+++ monkey/evaluator/evaluator.go
@@ -70,6 +70,16 @@ func Eval(node ast.Node, env *object.Enviroment) object.Object {
 
     case *ast.Identifier:
         return evalIdentifier(node, env)
+    case *ast.IndexExpression:
+        left := Eval(node.Left, env)
+        if isError(left) {
+            return left
+        }
+        index := Eval(node.Index, env)
+        if isError(index) {
+            return index
+        }
+        return evalIndexExpression(left, index)
 	// Exprss
     case *ast.ArrayLiteral:
         elements := evalExpressions(node.Elements, env)
@@ -86,6 +96,27 @@ func Eval(node ast.Node, env *object.Enviroment) object.Object {
 	}
 
 	return nil
+}
+
+func evalIndexExpression(left, index object.Object) object.Object {
+    switch {
+    case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
+        return evalArrayIndexExpression(left, index)
+    default:
+        return newError("index operator not supported: %s", left.Type())
+    }
+}
+
+func evalArrayIndexExpression(array, index object.Object) object.Object {
+    arrayObject := array.(*object.Array)
+    idx := index.(*object.Integer).Value
+    max := int64(len(arrayObject.Elements) - 1)
+
+    if idx < 0 || idx > max {
+        return NULL
+    }
+
+    return arrayObject.Elements[idx]
 }
 
 func applyFunction(fn object.Object, args []object.Object) object.Object {
--- monkey/lexer/lexer_test.go
+++ monkey/lexer/lexer_test.go
@@ -29,6 +29,7 @@ if (5 < 10) {
 "foobar"
 "foo bar"
 [1, 2];
+{"foo": "bar"}
 `
 
 	tests := []struct {
@@ -110,13 +111,17 @@ if (5 < 10) {
 		{token.SEMICOLON, ";"},
         {token.STRING, "foobar"},
         {token.STRING, "foo bar"},
-
         {token.LBRACKET, "["},
         {token.INT, "1"},
         {token.COMMA, ","},
         {token.INT, "2"},
         {token.RBRACKET, "]"},
         {token.SEMICOLON, ";"},
+        {token.LBRACE, "{"},
+        {token.STRING, "foo"},
+        {token.COLON, ":"},
+        {token.STRING, "bar"},
+        {token.RBRACE, "}"},
 
 		{token.EOF, ""},
 	}
